4611af1d7c5c82f37c5461360bab9de35a88d149
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2020-12-27 13:09:08 +0000

    Drawing the data
    I, in this commit, in total, did:
      -Drawing the data,
      -Stopping the program when it has finished reading Data,
      AND
      -Increased resolution

--- Code/sketch.js
+++ Code/sketch.js
@@ -4,14 +4,21 @@ let listPos = 0,
 
 function setup() {
   createCanvas(windowWidth - 20, windowHeight - 20);
-  for (let i = 0; i < TWO_PI; i += TWO_PI / 10) {
-    totalPoints.push(sin(i));
+  for (let i = 0; i < TWO_PI * 10; i += TWO_PI / 50) {
+    totalPoints.push(sin(i) * 100);
   }
+  stroke(255);
 }
 
 function draw() {
+  if (listPos > totalPoints.length - 1) {
+    noLoop();
+  }
   background(0);
   points.push(totalPoints[listPos]);
   listPos++;
-  listPos = constrain(listPos, 0, totalPoints.length - 1);
+
+  for (let i = 0; i < points.length; i++) {
+    line((width / points.length) * i, (height / 2) - points[i], (width / points.length) * (i - 1), (height / 2) - points[i - 1]);
+  }
 }
