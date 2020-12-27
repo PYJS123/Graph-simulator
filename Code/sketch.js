d80f520ccbb777aa0f0f43c73b1822c1541f59af
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2020-12-27 12:56:02 +0000

    Adding a system for data transfer

--- Code/sketch.js
+++ Code/sketch.js
@@ -1,7 +1,17 @@
+let listPos = 0,
+    points = [],
+    totalPoints = [];
+
 function setup() {
   createCanvas(windowWidth - 20, windowHeight - 20);
+  for (let i = 0; i < TWO_PI; i += TWO_PI / 10) {
+    totalPoints.push(sin(i));
+  }
 }
 
 function draw() {
   background(0);
+  points.push(totalPoints[listPos]);
+  listPos++;
+  listPos = constrain(listPos, 0, totalPoints.length - 1);
 }
